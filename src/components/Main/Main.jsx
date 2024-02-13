84948cd1cf9e86b1adc6edcf1944c4577ae30d38
NHanzhuha <NHanzhuha@healthprecision.com>
2024-02-12 19:12:14 -0700

    first page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,49 @@
+import yearsCard from '../../images/70th-logo.png';
+import blinksBottom from '../../images/blinks_bottom.png';
+import styles from './Main.module.css';
+import blinksVideo from '../../images/Happy_Birthday_Animation.mov';
+import { useEffect, useRef } from 'react';
+import blinksTop from '../../images/blinks-top.png';
+
 function Main() {
-  return <section>
-    <h1>Happy Birthday <b><u>Jonathan Aremu</u></b>!!!</h1>
+  const videoRef = useRef();
+
+  useEffect(() => {
+    const video = videoRef.current;
+    const handleEnded = () => {
+      video.play(); // Restart video when it ends
+    };
+
+    video.addEventListener('ended', handleEnded);
+
+    return () => {
+      video.removeEventListener('ended', handleEnded);
+    };
+  }, []);
+  return <section className="overflow-hidden">
+
+    <video ref={videoRef}
+           autoPlay
+           width="125%"
+           loop
+           muted>
+      <source src={blinksVideo}
+              type="video/mp4"/>
+      Your browser does not support the video tag.
+    </video>
+    {/*<div className={styles.shadow}></div>*/}
+    <img src={blinksTop}
+         alt="blinks"
+         className={styles.blinksTop}
+         width="100%"/>
+    <img src={yearsCard}
+         width='100%'
+         className={styles.yearImage}
+         alt="70 years birthday card"/>
+    <img src={blinksBottom}
+         alt="blinks"
+         width="100%"
+         className={styles.blinksBottom}/>
   </section>
 }
 
