e681be4769350e33f9b19e036745b973f948d6a6
NHanzhuha <NHanzhuha@healthprecision.com>
2024-02-20 09:53:10 -0700

    fixed party menu fixed main page top blinks

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,32 +2,18 @@ import yearsCard from '../../images/70th-logo.png';
 import preview from '../../images/video-preview.png';
 import styles from './Main.module.css';
 import blinksVideo from '../../images/Happy_Birthday_Animation.mov';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
 function Main() {
   const [loading, setLoadCount] = useState(true);
-  const videoRef = useRef();
-
-  useEffect(() => {
-    const video = videoRef.current;
-    const handleEnded = () => {
-      video.play(); // Restart video when it ends
-    };
-
-    video.addEventListener('ended', handleEnded);
-
-    return () => {
-      video.removeEventListener('ended', handleEnded);
-    };
-  }, []);
 
   return <section className={styles.section}>
     {loading && <img src={preview}
                      width="100%"
                      height="216px"
+                     className={styles.previewImage}
                      alt="video preview"/>}
-    <video ref={videoRef}
-           autoPlay
+    <video autoPlay
            className={styles.video}
            onLoadStart={() => setLoadCount(true)}
            onLoadedData={() => setLoadCount(false)}
--- src/components/PartyMenu/PartyMenu.jsx
+++ src/components/PartyMenu/PartyMenu.jsx
@@ -1,44 +1,64 @@
-import blinksBottom from '../../images/bottom.png';
 import styles from './PartyMenu.module.css';
 
 function PartyMenu() {
   const menuHash = {
-    'starter': [{
-      name: 'Crispy garlic breaded mushrooms',
-      description: 'backed in a creamy sauce topped with chives',
-    }],
-    'main': [{
-      name: 'tofu benedics fillo pockets',
-      description: 'with a splash of green pea puree, roasted garlic and fried mushrooms'
-    }],
-    'dessert': [{
-      name: 'nougat creme brulee with sea salt',
-      description: 'topped with hazelnuts'
-    }],
-    'drinks': [{name: 'pear and champagne cocktail'}, {name: 'blood orange champagne mule'}]
+    'starters': [
+      {name: 'Cat Fish Pepper soup', description: 'with bread rolls'},
+      {name: 'Goat Meat Pepper soup', description: 'with bread rolls'},
+      {name: 'Spring Rolls'},
+      {name: 'Samosa and Puff Puff'},
+      {name: 'Chicken Wings'},
+    ],
+    'main course': [
+      {name: 'Chinese Fried Rice', description: 'with mongolian beef sauce'},
+      {name: 'Singapore Noodles', description: 'with beef and broccoli sauce'},
+      {name: 'Offada Rice', description: 'with ayamase'},
+      {name: 'Efo Riro', description: 'with pounded yam'},
+      {name: 'Ogbono', description: 'with plantain flour'},
+      {name: 'Party Jollof Rice'},
+      {name: 'Avocado Salad'},
+    ],
+    'sides': [
+      {name: 'Moin Moin', description: '(steamed bean cakes)'},
+      {name: 'Coleslaw'},
+      {name: 'Plaintain'},
+      {name: 'Roasted Potato'},
+      {name: 'Sauteed Spinach'},
+    ],
+    'proteins': [
+      {name: 'Gizzard', description: "(bird's stomach)"},
+      {name: 'Chicken'},
+      {name: 'Fish'},
+      {name: 'Goat Meat'},
+    ],
+    'desserts': [
+      {name: 'Parfait'},
+      {name: 'Fruit Salad'}
+    ],
   };
+
   const menuHeaders = Object.keys(menuHash);
 
-  return <section className={styles.section} >
+  return <section className={styles.section}>
     <h1 className={styles.h1}>Party Menu</h1>
 
     <div className={styles.menuWrapper}>
       {menuHeaders.map((item, index) => {
-        return <div className={styles.typeWrapperStyles} key={item + index}>
+        return <div className={styles.typeWrapperStyles}
+                    key={item + index}>
           <h2 className={styles.typeHeaderStyles}>{item}</h2>
 
           {menuHash[item].map((dish, index) => {
-            return <div className={styles.dishWrapperStyles} key={index}>
+            return <div className={styles.dishWrapperStyles}
+                        key={index}>
               <h3 className={styles.dishNameStyle}>{dish.name}</h3>
               {dish.description && <i className="dish-description">{dish.description}</i>}
-            </div>
+            </div>;
           })}
-        </div>
+        </div>;
       })}
     </div>
-
-    <img src={blinksBottom} alt="bg" className="bg-bottom-image"/>
-  </section>
+  </section>;
 }
 
 export default PartyMenu;
