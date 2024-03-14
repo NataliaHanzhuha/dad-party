c20857628bd382dfc485b539d4c5125e0213b8d3
NHanzhuha <NHanzhuha@healthprecision.com>
2024-03-13 19:25:21 -0600

    fixed event page

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -2,18 +2,22 @@ import styles from './Events.module.css';
 
 function Events() {
   const eventHash = [
-    {time: '5 pm', events: [{name: 'Arrival of Guests'}]},
-    {time: '5 pm - 5:35 pm', events: [{name: 'Cocktail Hour'}, {name: '360 Video'}, {name: 'Cartoonist'}]},
-    {time: '5:40 pm', events: [{name: 'Guest gets seated'}]},
-    {time: '6 pm', events: [{name: 'Grand Entrance of the Celebrant'}]},
-    {time: '6:10 pm', events: [{name: 'Opening Prayers'}]},
+    {time: '4:00 pm', events: [{name: 'Arrival of Guests'}]},
+    {time: '4:00 pm - 4:35 pm',
+      events: [{name: 'Cocktail Hour'}, {name: '360 Video'}, {name: 'Cartoonist'}]},
+    {time: '4:45 pm', events: [{name: 'Guest gets seated'}]},
+    {time: '5:00 pm', events: [{name: 'Grand Entrance of the Celebrant'}]},
+    {time: '5:20 pm', events: [{name: 'Opening Prayers'}]},
+    {time: '5:20 pm - 5:30 pm', events: [{name: 'MC'}]},
     {time: '-', events: [{name: 'Speeches'}]},
-    {time: '6:30 pm', events: [{name: 'Dinner'}]},
-    {time: '7:15 pm', events: [{name: 'Cutting of the cake'}, {name: 'Photo session with guests'}]},
-    {time: '-', events: [{name: 'Celebrants special dance'}]},
+    {time: '-', events: [{name: 'Cutting of the cake'}, {name: 'Photo session with guests'}]},
+    {time: '5:40 pm', events: [{name: 'Dinner Dinner'}]},
+    {time: '6:30 pm - 6:45 pm', events: [{name: 'Presentation by the Children'}]},
+    {time: '6:50 pm - 6:55 pm', events: [{name: 'Celebrant special dance'}]},
     {time: '-', events: [{name: 'Prayer for the Celebrant and family'}]},
     {time: '-', events: [{name: 'Toast to the Celebrant'}]},
     {time: '-', events: [{name: 'Vote of Thanks by Celebrant'}]},
+    {time: '-', events: [{name: 'Closing Prayer'}]},
     {time: '-', events: [{name: 'Dance Dance Dance'}]},
   ];
 
