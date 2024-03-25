968f8a3ace9982dd146e6677340b1b952983f554
NHanzhuha <NHanzhuha@healthprecision.com>
2024-03-25 17:45:40 -0600

    fixed zoom for many images

--- src/components/Seats/Seats.jsx
+++ src/components/Seats/Seats.jsx
@@ -1,28 +1,11 @@
 import chart from '../../images/chart.png';
 import seats from '../../images/seats.png';
-import InnerImageZoom from 'react-inner-image-zoom';
-import { useRef, useCallback } from 'react';
-import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
+import Image from '../../utillits/Image/Image';
 
 function Seats() {
-    const imgRef = useRef();
-    const onUpdate = useCallback(({ x, y, scale }) => {
-      const { current: img } = imgRef;
-  
-      if (img) {
-        const value = make3dTransformValue({ x, y, scale });
-  
-        img.style.setProperty("transform", value);
-      }
-    }, []);
-
-    return <section>
-          <QuickPinchZoom onUpdate={onUpdate}>
-            <img ref={imgRef} src={seats} alt="seats"  width="100%"/>
-        </QuickPinchZoom>
-        {/* <InnerImageZoom src={seats} alt="seats" width="100%" zoomScale={'.15'} zoomType='hover'/> */}
-
-        <InnerImageZoom src={chart}  alt="chart" width="100%" zoomScale={'.15'} zoomType='hover'/>
+    return <section className='main'>
+        <Image src={seats} alt='seats'/>
+        <Image src={chart} alt='chart'/>
     </section>
 }
 
---
+++ src/utillits/Image/Image.jsx
@@ -0,0 +1,21 @@
+import { useRef, useCallback } from 'react';
+import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
+
+function Image({src, alt}) {
+    const imgRef = useRef();
+    const onUpdate = useCallback(({ x, y, scale }) => {
+      const { current: img } = imgRef;
+  
+      if (img) {
+        const value = make3dTransformValue({ x, y, scale });
+  
+        img.style.setProperty("transform", value);
+      }
+    }, []);
+
+    return <QuickPinchZoom onUpdate={onUpdate}>
+                <img ref={imgRef} src={src} alt={alt}  width="100%"/>
+            </QuickPinchZoom>
+}
+
+export default Image;
