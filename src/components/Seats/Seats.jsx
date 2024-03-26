fe4fc7ea3a81f91d82d85a3eb67f2c347051de18
NHanzhuha <NHanzhuha@healthprecision.com>
2024-03-26 15:03:03 -0600

    fixed for desctops

--- src/components/Seats/Seats.jsx
+++ src/components/Seats/Seats.jsx
@@ -5,17 +5,23 @@ import { Modal } from '../../utillits/Modal/Modal';
 import { useState } from 'react';
 
 function Seats() {
-    const [fullSizePhoto, toggleFullSizePhoto] = useState(null);
-    const fullSizeImage = <Image src={fullSizePhoto} alt='seats'/>;
-  
-    return<><section className='main seats'>
-        <img src={seats} alt='seats'  onClick={() => toggleFullSizePhoto(seats)}/>
-        <img src={chart} alt='chart'  onClick={() => toggleFullSizePhoto(chart)}/>
+  const [fullSizePhoto, toggleFullSizePhoto] = useState(null);
+  const fullSizeImage = <Image src={fullSizePhoto}
+                               alt="seats"/>;
+
+  return <>
+    <section className="main seats">
+      <img src={seats}
+           alt="seats"
+           onClick={() => toggleFullSizePhoto(seats)}/>
+      <img src={chart}
+           alt="chart"
+           onClick={() => toggleFullSizePhoto(chart)}/>
     </section>
     {fullSizePhoto && <Modal closeModal={() => toggleFullSizePhoto(null)}
                              header="Photo"
                              content={fullSizeImage}></Modal>}
-</>;
+  </>;
 }
 
 export default Seats;
