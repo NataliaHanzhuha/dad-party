d967d96102271ff1442f30ef9b7a783b4a8dfae1
NHanzhuha <NHanzhuha@healthprecision.com>
2024-03-26 14:59:05 -0600

    fixed image zoom

--- src/components/Seats/Seats.jsx
+++ src/components/Seats/Seats.jsx
@@ -1,20 +1,21 @@
 import chart from '../../images/chart.png';
 import seats from '../../images/seats.png';
 import Image from '../../utillits/Image/Image';
+import { Modal } from '../../utillits/Modal/Modal';
+import { useState } from 'react';
 
 function Seats() {
-    // const styles = {
-    //     overflowY: 'scroll',
-    //     width: '100%',
-    //     height: '10000px',
-    //     display: 'block',
-    //     marginBottom: '100px',
-    // }
+    const [fullSizePhoto, toggleFullSizePhoto] = useState(null);
+    const fullSizeImage = <Image src={fullSizePhoto} alt='seats'/>;
   
-    return <section className='main seats'>
-        <Image src={seats} alt='seats'/>
-        <Image src={chart} alt='chart'/>        
-    </section>;
+    return<><section className='main seats'>
+        <img src={seats} alt='seats'  onClick={() => toggleFullSizePhoto(seats)}/>
+        <img src={chart} alt='chart'  onClick={() => toggleFullSizePhoto(chart)}/>
+    </section>
+    {fullSizePhoto && <Modal closeModal={() => toggleFullSizePhoto(null)}
+                             header="Photo"
+                             content={fullSizeImage}></Modal>}
+</>;
 }
 
 export default Seats;
--- src/utillits/Image/Image.jsx
+++ src/utillits/Image/Image.jsx
@@ -1,21 +1,28 @@
-import { useRef, useCallback } from 'react';
-import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
+import { useCallback, useRef } from 'react';
+import QuickPinchZoom, { make3dTransformValue } from 'react-quick-pinch-zoom';
 
 function Image({src, alt}) {
-    const imgRef = useRef();
-    const onUpdate = useCallback(({ x, y, scale }) => {
-      const { current: img } = imgRef;
-  
-      if (img) {
-        const value = make3dTransformValue({ x, y, scale });
-  
-        img.style.setProperty("transform", value);
-      }
-    }, []);
+  const imgRef = useRef();
+  const onUpdate = useCallback(({x, y, scale}) => {
+    const {current: img} = imgRef;
 
-    return <QuickPinchZoom onUpdate={onUpdate}>
-                <img ref={imgRef} src={src} alt={alt} className="zoom-image"/>
-            </QuickPinchZoom>
+    if (img) {
+      const value = make3dTransformValue({x, y, scale});
+      img.style.setProperty('transform', value);
+    }
+  }, []);
+
+  return <QuickPinchZoom onUpdate={onUpdate}
+                         tapZoomFactor={2}
+                         zoomOutFactor={4}
+                         inertiaFriction={0}
+                         maxZoom={10}
+                         minZoom={1}>
+    <img ref={imgRef}
+         src={src}
+         alt={alt}
+         className="zoom-image"/>
+  </QuickPinchZoom>;
 }
 
 export default Image;
